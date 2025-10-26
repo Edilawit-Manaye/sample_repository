12f949d03a90c4790f564ee205cbef7a9ec23eed
Edilawit-Manaye <fortune124a@gmail.com>
2025-10-26 18:48:10 +0300

    Add files via upload

---
+++ docs/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val numberSets = listOf(setOf(1, 2, 3), setOf(4, 5), setOf(1, 2))
+    println(numberSets)
+
+
+
+}
